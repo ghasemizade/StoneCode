2448f17756da2f393013801d2ae1c104208492c4
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2026-03-24 21:50:59 +0330

    add ex6 file

---
+++ tasks/js-tasks/Ex6/app.js
@@ -0,0 +1,9 @@
+let firstNumber = Number(prompt("enter first number:"), 1)
+let secondNumber = Number(prompt("enter second number:"), 1)
+
+const Pow = (a, b) => {
+    return a ** b;
+}
+
+
+console.log(Pow(firstNumber, secondNumber));
